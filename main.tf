0eeec18b41f73e858a5c4f44210297d8d4c56abf
Akshay-bl <lattheakshay02@gmail>
2024-02-08 20:51:37 +0530

    demo update

--- main.tf
+++ main.tf
@@ -27,10 +27,6 @@ resource "spacelift_policy" "illegal_ports" {
   type = "PLAN"
 }
 
-resource "spacelift_policy_attachment" "illegal_ports_attachment" {
-  policy_id = spacelift_policy.illegal_ports.id
-  stack_id  = spacelift_stack.main_stack.id
-}
 
 resource "spacelift_policy" "enforce_cloud_provider" {
   name = "Policy-02"
@@ -38,10 +34,6 @@ resource "spacelift_policy" "enforce_cloud_provider" {
   type = "PLAN"
 }
 
-resource "spacelift_policy_attachment" "enforce_cloud_provider_attachment" {
-  policy_id = spacelift_policy.enforce_cloud_provider.id
-  stack_id  = spacelift_stack.main_stack.id
-}
 
 resource "spacelift_policy" "instance_size_policy" {
   name = "Policy-03"
@@ -49,11 +41,31 @@ resource "spacelift_policy" "instance_size_policy" {
   type = "PLAN"
 }
 
+
+resource "spacelift_policy_attachment" "illegal_ports_attachment" {
+  policy_id = spacelift_policy.illegal_ports.id
+  stack_id  = spacelift_stack.main_stack.id
+}
+resource "spacelift_policy_attachment" "illegal_ports_attachment_dev" {
+  policy_id = spacelift_policy.illegal_ports.id
+  stack_id  = spacelift_stack.dev_stack.id
+}
+
+resource "spacelift_policy_attachment" "enforce_cloud_provider_attachment" {
+  policy_id = spacelift_policy.enforce_cloud_provider.id
+  stack_id  = spacelift_stack.main_stack.id
+}
+
+resource "spacelift_policy_attachment" "enforce_cloud_provider_attachment_dev" {
+  policy_id = spacelift_policy.enforce_cloud_provider.id
+  stack_id  = spacelift_stack.dev_stack.id
+}
+
+
 resource "spacelift_policy_attachment" "instance_size_policy_attachment" {
   policy_id = spacelift_policy.instance_size_policy.id
   stack_id  = spacelift_stack.main_stack.id
 }
-
 
 resource "spacelift_policy_attachment" "instance_size_policy_attachment_dev" {
   policy_id = spacelift_policy.instance_size_policy.id
