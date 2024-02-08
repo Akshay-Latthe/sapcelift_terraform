21585c05f31bf4cde31f1a33d7ed66b3fef33283
Akshay-bl <lattheakshay02@gmail>
2024-02-08 18:50:49 +0530

    this stack update

--- main.tf
+++ main.tf
@@ -1,35 +1,34 @@
 
+resource "spacelift_stack" "example-stack" {
+  name = var.new_stack_name
+  administrative    = true
+  autodeploy        = false
+  branch            = "master"
+  description       = "Shared production infrastructure"
+  repository        = "testing-spacelift"
+  terraform_version = "0.12.27"
+}
+
+
 resource "spacelift_stack" "main_stack" {
   name       = var.main_stack
   branch     = "main"
   repository = "sapcelift_with_terraform"
 }
+
 resource "spacelift_stack" "dev_stack" {
   name       = var.dev_stack
   branch     = "dev"
   repository = "sapcelift_with_terraform"
 }
 
-resource "spacelift_stack" "example-stack" {
-  name = var.new_stack_name
-  administrative    = true
-  autodeploy        = false
-  branch            = "master"
-  description       = "Shared production infrastructure (networking, k8s)"
-  repository        = "testing-spacelift"
-  terraform_version = "0.12.27"
-}
 
+#==============Policy ===================================
 
 resource "spacelift_policy" "illegal_ports" {
   name = "Policy-01"
   body = file("${path.module}/policy/no-illegal-ports.rego")
   type = "PLAN"
-}
-
-resource "spacelift_policy_attachment" "illegal_ports_attachment" {
-  policy_id = spacelift_policy.illegal_ports.id
-  stack_id  = spacelift_stack.main_stack.id
 }
 
 resource "spacelift_policy" "enforce_cloud_provider" {
@@ -38,18 +37,53 @@ resource "spacelift_policy" "enforce_cloud_provider" {
   type = "PLAN"
 }
 
-resource "spacelift_policy_attachment" "enforce_cloud_provider_attachment" {
-  policy_id = spacelift_policy.enforce_cloud_provider.id
-  stack_id  = spacelift_stack.main_stack.id
-}
-
 resource "spacelift_policy" "instance_size_policy" {
   name = "Policy-03"
   body = file("${path.module}/policy/instance-size-policy.rego")
   type = "PLAN"
 }
 
+
+#==============Attach Policy to stack "example-stack" ===================================
+resource "spacelift_policy_attachment" "illegal_ports_attachment" {
+  policy_id = spacelift_policy.illegal_ports.id
+  stack_id  = spacelift_stack.example-stack.id
+}
+resource "spacelift_policy_attachment" "enforce_cloud_provider_attachment" {
+  policy_id = spacelift_policy.enforce_cloud_provider.id
+  stack_id  = spacelift_stack.example-stack.id
+}
+resource "spacelift_policy_attachment" "instance_size_policy_attachment" {
+  policy_id = spacelift_policy.instance_size_policy.id
+  stack_id  = spacelift_stack.example-stack.id
+}
+
+#==============Attach Policy to stack "main_stack" ======================================
+resource "spacelift_policy_attachment" "illegal_ports_attachment" {
+  policy_id = spacelift_policy.illegal_ports.id
+  stack_id  = spacelift_stack.main_stack.id
+}
+resource "spacelift_policy_attachment" "enforce_cloud_provider_attachment" {
+  policy_id = spacelift_policy.enforce_cloud_provider.id
+  stack_id  = spacelift_stack.main_stack.id
+}
 resource "spacelift_policy_attachment" "instance_size_policy_attachment" {
   policy_id = spacelift_policy.instance_size_policy.id
   stack_id  = spacelift_stack.main_stack.id
+}
+
+
+#==============Attach Policy to stack "dev_stack" ====================================
+
+resource "spacelift_policy_attachment" "illegal_ports_attachment" {
+  policy_id = spacelift_policy.illegal_ports.id
+  stack_id  = spacelift_stack.dev_stack.id
+}
+resource "spacelift_policy_attachment" "enforce_cloud_provider_attachment" {
+  policy_id = spacelift_policy.enforce_cloud_provider.id
+  stack_id  = spacelift_stack.dev_stack.id
+}
+resource "spacelift_policy_attachment" "instance_size_policy_attachment" {
+  policy_id = spacelift_policy.instance_size_policy.id
+  stack_id  = spacelift_stack.dev_stack.id
 }
